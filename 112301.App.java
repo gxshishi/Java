f8ede1e211a4a3ec8ef92d9c65b6e8648d238de0
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-11-23 12:00:29 +0800

    Create 112301.App.java
    随机生成坐标java程序

---
+++ 112301.App.java
@@ -0,0 +1,22 @@
+package pac;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("欢迎来到随机坐标生成小程序");
+		System.out.println("请输入一个数字：");
+		Scanner scanner=new Scanner(System.in);
+		while (true) {
+		int shishi=scanner.nextInt();
+		if (shishi==1) {
+			int x=(int)(Math.random()*shishi*100%100);
+			int y=(int)(Math.random()*shishi*100%100);
+			System.out.println("随机生成的二维坐标是：("+x+","+y+")");
+		}else {
+			System.out.println("程序发生错误，请输入数字1");
+			}
+		}
+	
+	}
+}
