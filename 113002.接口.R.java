f8e0f9d0f4f6be0e1240aaff14bda48a68614d88
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-11-30 11:53:04 +0800

    接口

--- 113002.接口.R.java
+++ 113002.接口.R.java
@@ -4,10 +4,20 @@ public interface R {
 	static void ShiShi() {
 		String boy_filePath="img/boy.gif";
 		String girl_filePath="img/girl.gif";
-		int[][] array2d= {{1,2,3},{4,5,6}};
+		int[][] array2d= {
+				{1,2},
+				{3,4},
+				{5,6}
+				};
+		
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		System.out.println(array2d[0][1]);
 		
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.print(array2d[row][column]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
