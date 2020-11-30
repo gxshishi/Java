0f740e84c4d269c57e2658b5ef14908aa275c4e3
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-11-30 11:39:41 +0800

    Create 113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,12 @@
+package pac;
+
+public interface R {
+	static void ShiShi() {
+		String boy_filePath="img/boy.gif";
+		String girl_filePath="img/girl.gif";
+		int[][] array2d= {{1,2,3},{4,5,6}};
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d[0][1]);
+	}
+}
