cf607e396220608fea0c175c28ebea7b39b268d7
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-11-30 13:53:03 +0800

    接口

--- 113002.接口.R.java
+++ 113002.接口.R.java
@@ -4,7 +4,8 @@ public interface R {
 	static void ShiShi() {
 		String boy_filePath="img/boy.gif";
 		String girl_filePath="img/girl.gif";
-
+		int [][]array2d={{1,2},{3,4},{5,6}};
+		
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
 	}
