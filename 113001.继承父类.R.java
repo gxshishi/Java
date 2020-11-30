26ead756cc1d44665bbfd1817b254867e5f632ec
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-11-30 11:52:27 +0800

    继承父类

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -4,9 +4,20 @@ public class R {
 	static void ShiShi() {
 		String boy_filePath="img/boy.gif";
 		String girl_filePath="img/girl.gif";
-		int[][] array2d= {{1,2,3},{4,5,6}};
+		int[][] array2d= {
+				{1,2},
+				{3,4},
+				{5,6}
+				};
+		
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		System.out.println(array2d[1][2]);
+		
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.print(array2d[row][column]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
