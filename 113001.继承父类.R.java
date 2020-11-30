cc094da21e4a0b6b6f170c528168a58dcde495bd
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-11-30 11:38:24 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,14 @@
+package pac;
+
+public class R {
+	static void ShiShi() {
+		String boy_filePath="img/boy.gif";
+		String girl_filePath="img/girl.gif";
+		int[][] array2d= {{1,2,3},{4,5,6}};
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d[1][2]);
+		
+
+	}
+}
