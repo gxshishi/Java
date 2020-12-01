1801daf9e0d8a2cf85e4fe938217009955f63e7b
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-12-01 23:27:48 +0800

    继承子类

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -2,17 +2,15 @@ package pac;
 
 public class App extends R {
 	public static void main(String[] args) {
-		ShiShi();
-		int[][] array2d= {
-				{1,2},
-				{3,4},
-				{5,6}
-				};
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
 		for (int row = 0; row < array2d.length; row++) {
 			for (int column = 0; column < array2d[0].length; column++) {
 				System.out.print(array2d[row][column]+" ");
 			}
 			System.out.println();
 		}
+		
 	}
 }
+
