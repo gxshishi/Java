2fb437bfb14ccaf92dd37f6ba250bab2b614d7f7
gxshishi <74659818+gxshishi@users.noreply.github.com>
2020-12-01 23:31:51 +0800

    接口

--- 113002.接口.R.java
+++ 113002.接口.R.java
@@ -1,12 +1,11 @@
 package pac;
 
 public interface R {
-	static void ShiShi() {
-		String boy_filePath="img/boy.gif";
-		String girl_filePath="img/girl.gif";
-		int [][]array2d={{1,2},{3,4},{5,6}};
-		
-		System.out.println(boy_filePath);
-		System.out.println(girl_filePath);
-	}
+		static String boy_filePath="img/boy.gif";
+		static String girl_filePath="img/girl.gif";
+		static int[][] array2d= {
+				{1,2},
+				{3,4},
+				{5,6}
+				};	
 }
